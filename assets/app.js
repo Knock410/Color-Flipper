304c30d59b4c3d4bf1a6a285cd2c0e52a3b48695
Knock <enoch10evelyn@gmail.com>
2023-10-15 13:32:20 -0400

    edited script placement

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,12 @@
-console.log("Kick");
+// console.log("Kick");
+
+const colors = [ "green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector("color");
+
+
+btn.addEventListener("click", function(){
+//get random number berwwen 0-3 colors[3]
+const randomNumber = 2;
+});
